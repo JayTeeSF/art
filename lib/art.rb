0d88423424f37a6659fd69168ad748fa3d2f0ab4
jayteesf <jayteesf>
2013-07-07 21:32:15 -0700

    hackery (tb-removed); testing animation possibilities ( bike rider)

--- lib/art.rb
+++ lib/art.rb
@@ -27,6 +27,7 @@ class Art
   end
 
   def move_to x, y
+    x = x.to_i; y = y.to_i
     if !within_bounds?( x, y )
       raise RuntimeError, "Out of bounds: #{x.inspect}, #{y.inspect}"
     end
@@ -35,6 +36,7 @@ class Art
   end
 
   def line_to x, y, char=DEFAULT_CHAR
+    x = x.to_i; y = y.to_i
     if !within_bounds?( x, y )
       raise RuntimeError, "Out of bounds: #{x.inspect}, #{y.inspect}"
     end
@@ -51,13 +53,17 @@ class Art
   end
 
   def type( message=DEFAULT_CHAR, pace=0.3, at=current_position )
-   message.split(//).each do |char|
-     at.increment_x
-     last unless within_bounds?( *at.to_a )
-     draw(char, at)
-     display
-     sleep( pace ) if pace
-   end
+    pace = Float(pace) if pace
+    message.split(//).each do |char|
+      at.increment_x
+      if within_bounds?( *at.to_a )
+        draw(char, at)
+        if pace
+          display
+          sleep( pace )
+        end
+      end
+    end
   end
 
   def draw( pixel, at=current_position )
@@ -73,21 +79,125 @@ class Art
     reset_position
   end
 
+  def ride_to(destination_x, pace=0.3)
+    destination_x = destination_x.to_i
+    pace = Float(pace)
+    
+    current_y = current_position.y
+    current_x = current_position.x
+    distance = destination_x - current_x
+    distance = -distance if distance < 0
+    (0..distance).each do |peddle|
+      if peddle > 1
+        bike( current_x + (peddle - 1), current_y, 'blank_fast' )
+        bike( current_x + peddle, current_y, 'fast' )
+        pace = pace - 0.1
+        pace = 0.1 if pace < 0.1
+      elsif peddle > 0
+        bike( current_x + (peddle - 1), current_y, 'blank' )
+        bike( current_x + peddle, current_y, 'fast' )
+      else
+        bike( current_x + peddle, current_y )
+      end
+      display
+      sleep(pace)
+    end
+  end
+
+  #def blank_bike(x, y)
+  #  move_to(x, y)
+  #  write blank_bike_line_one
+  #  move_to(x, y+1)
+  #  write blank_bike_line_two
+  #  move_to(x, y+2)
+  #  write blank_bike_line_three
+  #end
+
+  def bike(x, y, type=nil)
+    x = x.to_i; y = y.to_i
+    move_to(x, y)
+    method_name = type.nil? ? :bike_line_one : :"#{type}_bike_line_one"
+    method = method(method_name)
+    write method.call
+    move_to(x, y+1)
+    method_name = type.nil? ? :bike_line_two : :"#{type}_bike_line_two"
+    method = method(method_name)
+    write method.call
+    move_to(x, y+2)
+    #write bike_line_three
+    method_name = type.nil? ? :bike_line_three : :"#{type}_bike_line_three"
+    method = method(method_name)
+    write method.call
+  end
+
+  def blank_bike_line_one
+    '       '
+  end
+
+  def blank_bike_line_two
+    '        '
+  end
+
+  def blank_bike_line_three
+    '         '
+  end
+
+  def bike_line_one
+    '    __o'
+  end
+
+  def bike_line_two
+    ['  _ \<,_', '  -\<,' ][rand(2)]
+  end
+
+  def bike_line_three
+    ['(_)/  (_)', 'O / O'][rand(2)]
+  end
+
+  def fast_bike_line_one
+    ' ____   __o'
+  end
+
+  def fast_bike_line_two
+    [ '____  _-\_<,', '____  _ \<,_', '____    -\<,' ][rand(3)]
+  end
+
+  def fast_bike_line_three
+    # ["     (*)/'(*)", '      O / O'][rand(2)]
+    "     (*)/'(*)"
+  end
+
+  def blank_fast_bike_line_one
+    '           '
+  end
+
+  def blank_fast_bike_line_two
+    '            '
+  end
+
+  def blank_fast_bike_line_three
+    '             '
+  end
+
   # height = 4
   # width = 2
   # start in lower-left
-  def triangle(start_x, start_y, width, height)
-     move_to(start_x, start_y)
-     line_to(start_x + 2 * width, start_y)
-     line_to(start_x + width, start_y - height)
-     line_to(start_x, start_y)
+  def triangle(start_x, start_y, width, height, char=DEFAULT_CHAR)
+    start_x = start_x.to_i; start_y = start_y.to_i
+    width = width.to_i; height = height.to_i
+    move_to(start_x, start_y)
+    line_to(start_x + 2 * width, start_y, char)
+    line_to(start_x + width, start_y - height, char)
+    line_to(start_x, start_y, char)
   end
-  def square(start_x, start_y, width, height)
-     move_to(start_x, start_y)
-     line_to(start_x + width, start_y)
-     line_to(start_x + width,start_y + height)
-     line_to(start_x,start_y + height)
-     line_to(start_x, start_y)
+  def square(start_x, start_y, width, height, char=DEFAULT_CHAR)
+    start_x = start_x.to_i; start_y = start_y.to_i
+    width = width.to_i; height = height.to_i
+    move_to(start_x, start_y)
+    line_to(start_x + width, start_y, char)
+    line_to(start_x + width,start_y + height, char)
+    line_to(start_x,start_y + height, char)
+    line_to(start_x, start_y, char)
   end
 
   private
@@ -100,8 +210,8 @@ class Art
 
   def within_bounds?( x, y )
     canvas.min_x <= x &&
-    x <= canvas.max_x &&
-    canvas.min_y <= y &&
-    y <= canvas.max_y
+      x <= canvas.max_x &&
+      canvas.min_y <= y &&
+      y <= canvas.max_y
   end
 end
